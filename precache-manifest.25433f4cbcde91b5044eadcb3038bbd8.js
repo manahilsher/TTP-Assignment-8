3c34e0366aa97f7c2048de407c10a586a9f499b9
Manahil Sher <manahil.sher@gmail.com>
2020-06-08 14:08:14 -0400

    Updates

---
+++ precache-manifest.25433f4cbcde91b5044eadcb3038bbd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "623efdf75d27a61c131ea7bb5dc26d83",
+    "url": "/TTP-Assignment-8/index.html"
+  },
+  {
+    "revision": "7bd1464b4601461f7350",
+    "url": "/TTP-Assignment-8/static/css/main.5bf7de5b.chunk.css"
+  },
+  {
+    "revision": "180eea1fe435731124ac",
+    "url": "/TTP-Assignment-8/static/js/2.1a472dc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TTP-Assignment-8/static/js/2.1a472dc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd1464b4601461f7350",
+    "url": "/TTP-Assignment-8/static/js/main.1489a397.chunk.js"
+  },
+  {
+    "revision": "b78f77d5781991e1033c",
+    "url": "/TTP-Assignment-8/static/js/runtime-main.2fdc400d.js"
+  }
+]);
